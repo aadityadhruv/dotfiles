7a850689283acb71be6dca5c8a9fd232bb790bdb
Aaditya Dhruv <aadityadhruv@mailbox.org>
2022-07-05 14:04:20 -0700

    firefox

--- tools.sh
+++ tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S bspwm sxhkd polybar rofi picom git lightdm bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty xfce-power-manager blueman lightdm-gtk-greeter arandr nitrogen 
+sudo pacman -S firefox xorg-server bspwm sxhkd polybar rofi picom git lightdm bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty xfce-power-manager blueman lightdm-gtk-greeter arandr nitrogen 
