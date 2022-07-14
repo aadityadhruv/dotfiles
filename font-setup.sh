e5c0fb651869118e8e3bfcd46b07448911cb46fd
Aaditya Dhruv <aadityadhruv@mailbox.org>
2022-07-14 13:13:36 -0700

    font fix

--- font-setup.sh
+++ font-setup.sh
@@ -2,3 +2,5 @@ echo "Making fonts directory..."
 mkdir -p ~/.local/share/fonts/
 echo "Moving Meslo fonts to ~/.local/share/fonts ..."
 mv Meslo* ~/.local/share/fonts
+
+sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
