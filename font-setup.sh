8da44ac5ae0aaa28caa79c2188e3a2b0249802da
Aaditya Dhruv <67942447+aadityadhruv@users.noreply.github.com>
2022-07-15 22:48:33 -0700

    mv -> cp for fonts

--- font-setup.sh
+++ font-setup.sh
@@ -1,6 +1,6 @@
 echo "Making fonts directory..."
 mkdir -p ~/.local/share/fonts/
 echo "Moving Meslo fonts to ~/.local/share/fonts ..."
-mv Meslo* ~/.local/share/fonts
+cp Meslo* ~/.local/share/fonts
 
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
