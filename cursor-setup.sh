d69ec4f3bffc17b67944a56bb1e71eb934246e83
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-01-09 17:09:56 +0530

    scripts fix

--- cursor-setup.sh
+++ cursor-setup.sh
@@ -1,4 +1,5 @@
 echo "extracting Simple Dark to ~/.local/share/icons"
+mkdir ~/.local/share/icons -p
 tar xvf Simp1e-Dark.tar.gz -C ~/.local/share/icons
 
 echo "Making cursor default theme file at ~/.icons/default/..."
@@ -6,12 +7,12 @@ mkdir ~/.icons/default/ -p
 touch ~/.icons/default/index.theme
 
 echo "Setting theme..."
-echo "[icon theme]\nInherits=Simp1e-Dark" > ~/.icons/default/index.theme
+echo -e "[icon theme]\nInherits=Simp1e-Dark" > ~/.icons/default/index.theme
 
 echo "GTK theme change..."
 mkdir ~/.config/gtk-3.0/ -p
 touch ~/.config/gtk-3.0/settings.ini
-echo "[Settings]\ngtk-cursor-theme-name=Simp1e-Dark" > ~/.config/gtk-3.0/settings.ini
+echo -e "[Settings]\ngtk-cursor-theme-name=Simp1e-Dark" > ~/.config/gtk-3.0/settings.ini
 
 
 
--- zsh-setup.sh
+++ zsh-setup.sh
@@ -8,5 +8,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 
 cp .zshrc .p10k ~/
