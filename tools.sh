f0a22e8a3ef8ea9db76653962a9c32b01aee7066
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-03-24 00:14:43 -0500

    wayland tools

--- tools.sh
+++ tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S wine wine-mono winetricks dunst steam htop thunderbird bitwarden firefox xorg-server bspwm sxhkd polybar rofi picom git lightdm bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty xfce-power-manager blueman lightdm-gtk-greeter arandr nitrogen lxapperance playerctl
+sudo pacman -S wine wine-mono winetricks dunst steam htop thunderbird bitwarden firefox xorg-server bspwm sxhkd polybar rofi picom git lightdm bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty xfce-power-manager blueman lightdm-gtk-greeter arandr nitrogen lxapperance playerctl hyperland-git waybar
 
 
 sudo xbps-install wine wine-mono winetricks dunst steam htop thunderbird bitwarden firefox xorg bspwm sxhkd polybar rofi picom git lightdm bluez pipewire brightnessctl alacritty xfce4-power-manager blueman lightdm-gtk3-greeter arandr nitrogen pcmanfm NetworkManager
