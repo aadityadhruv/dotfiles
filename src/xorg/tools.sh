bd62baf6409f71ba3ceadfb92604b38f260e3374
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-09-09 18:28:57 -0500

    Removed xorg directory to favor organization in playbooks instead
    Instead of splitting data by directories in the repo, playbooks will be
    used to handle whether to install for xorg, wayland etc.

--- src/xorg/tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S wine wine-mono winetricks dunst steam htop thunderbird firefox xorg-server bspwm sxhkd polybar rofi picom git lightdm bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty xfce-power-manager blueman lightdm-gtk-greeter arandr nitrogen lxapperance playerctl 
-
-
-sudo xbps-install wine wine-mono winetricks dunst steam htop thunderbird bitwarden firefox xorg bspwm sxhkd polybar rofi picom git lightdm bluez pipewire brightnessctl alacritty xfce4-power-manager blueman lightdm-gtk3-greeter arandr nitrogen pcmanfm NetworkManager
-
-
-sudo xbps-install wine-32bit void-repo-multilib{,-nonfree} legendary python3 python3-pip libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mesa-dri-32bit mesa-dri mesa-32bit
