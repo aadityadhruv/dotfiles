209f13573abb0e69738458a5d587a5af07abdbff
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-09-09 18:25:17 -0500

    Removed script to install tools/packages
    This was a crappy script that ran a package manager command in one line
    and made it hard to edit and classify what packages are being installed.
    This is being replaced with a organized YAML file as a source for
    packages and a playbook to install them

--- src/wayland/tools.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S wine wine-mono winetricks dunst steam btop thunderbird firefox rofi git bluez pipewire pipewire-jack pipewire-alsa pipewire-pulse brightnessctl alacritty bluetoothctl playerctl hyperland-git waybar
