5fcb99d7c16029ee0495a437a7bbe7b003dda023
Aaditya Dhruv <aadityadhruv@mailbox.org>
2025-06-06 11:16:42 -0500

    Cleanup configs

--- src/config/files/fonts/fonts.sh
+++
@@ -1,6 +0,0 @@
-echo "Making fonts directory..."
-mkdir -p ~/.local/share/fonts/ -p
-echo "Moving Meslo fonts to ~/.local/share/fonts ..."
-cp ./MesloLGS/* ~/.local/share/fonts/
-
-sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
