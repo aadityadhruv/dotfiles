e8f499fbe659fc9b5b9bbdca29f6396b8584ae5f
Aaditya Dhruv <aadityadhruv@mailbox.org>
2025-06-30 13:24:02 -0500

    Add git related aliases

--- src/config/files/shell/bashrc.d/20-aliases.sh
+++ src/config/files/shell/bashrc.d/20-aliases.sh
@@ -3,5 +3,13 @@
 alias vi=nvim
 alias k=kubectl
 alias tmux="TERM=xterm-256color tmux"
-alias note="vi $HOME/Notes/general/\$(get_date).md"
 alias get_idf='. $HOME/git/tools/esp-idf/export.sh'
+alias gl='git pull'
+alias glo='git log'
+alias gp='git push'
+alias gst='git status'
+alias gsw='git switch'
+alias gc='git commit'
+alias ga='git add'
+alias gr='git rebase'
+
