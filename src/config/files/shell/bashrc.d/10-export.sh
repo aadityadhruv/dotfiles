bdd22355a22532255e9a57038ca07254533d6d20
Aaditya Dhruv <aadityadhruv@mailbox.org>
2025-12-07 16:39:29 -0600

    Minor patches to config files, updates to repofile

--- src/config/files/shell/bashrc.d/10-export.sh
+++ src/config/files/shell/bashrc.d/10-export.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-eval "$(starship init bash)"
+export HISTSIZE=10000
+export HISTFILESIZE=10000
 export DISABLE_MAGIC_FUNCTIONS=true
 export BROWSER=/usr/bin/firefox
 export EDITOR="/usr/bin/nvim"
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 export GPG_TTY=$(tty)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 source <(kubectl completion bash)
 source <(fzf --bash)
+eval "$(starship init bash)"
