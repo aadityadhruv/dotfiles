4beb2247b5af0da4b4b62000e6e0ebf29519c332
Aaditya Dhruv <aadityadhruv@mailbox.org>
2026-07-07 19:32:46 +0200

    Ensure install directories exist, move some vimrc config to init.lua

--- src/config/files/neovim/lua/nvim.lua
+++ src/config/files/neovim/lua/nvim.lua
@@ -1,3 +1,22 @@
+vim.opt.bs = 2
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth= 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.t_Co = 256
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.cursorline = true
+vim.opt.mouse = "vn"
+vim.opt.scrolloff = 5
+vim.opt.completeopt = vim.opt.completeopt + { "noselect", "menuone" }
+vim.api.nvim_create_autocmd('BufReadPre', {
+    pattern = { "*.md", "*.txt" },
+  callback = function()
+      vim.opt_local.textwidth = 80
+  end,
+})
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
@@ -17,6 +36,8 @@ end)
 vim.keymap.set('n', 'gd', function()
   vim.diagnostic.open_float()
 end)
+
+
 
 vim.pack.add({
     'https://github.com/stevearc/oil.nvim',
